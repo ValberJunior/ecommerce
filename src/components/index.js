69909f8fc8314022e3f4aac0e546058dff8c8351
ValberJunior <valberjr1992@gmail.com>
2021-12-16 17:34:52 -0300

    Create Announcement Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as GlobalStyle } from './globalStyle/GlobalStyle';
 export { default as Navbar } from './navbar/Navbar';
+export { default as Announcement } from './announcement/Announcement';
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Navbar } from '../../components';
+import { Announcement, Navbar } from '../../components';
 
 const Home = () => {
     return (
         <div>
+            <Announcement/>
             <Navbar/>
         </div>
     )
