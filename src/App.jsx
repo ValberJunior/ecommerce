db51de40e63611b0928f372429b6d1d9712ab885
ValberJunior <valberjr1992@gmail.com>
2021-12-18 00:38:04 -0300

    Create ProductList Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { Home } from './pages';
+import { Home, ProductList } from './pages';
 import { theme } from './theme';
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Home/>
+      {/* <Home/> */}
+      <ProductList/>
     </ThemeProvider>
   );
 }
---
+++ src/pages/productList/ProductList.jsx
@@ -0,0 +1,53 @@
+import { Container, Filter, FilterContainer, FilterText, Option, Select, Title } from './styles'
+import { Announcement, Footer, Navbar, Newsletter, Products } from '../../components';
+ 
+const styles = () => {
+    return (
+        <Container>
+            <Navbar/>
+            <Announcement/>
+
+            <Title>Dresses</Title>
+
+            <FilterContainer>
+                <Filter>
+                    <FilterText>Filter Products:</FilterText>
+                    <Select>
+                        <Option disable selected>Color</Option>
+                        <Option>White</Option>
+                        <Option>Black</Option>
+                        <Option>Red</Option>
+                        <Option>Blue</Option>
+                        <Option>Yellow</Option>
+                        <Option>Green</Option>
+                    </Select>
+
+                    <Select>
+                        <Option disable selected>Size</Option>
+                        <Option>XS</Option>
+                        <Option>S</Option>
+                        <Option>M</Option>
+                        <Option>L</Option>
+                        <Option>XL</Option>
+                    </Select>
+                </Filter>
+
+                <Filter>
+                    <FilterText>Sort Products:</FilterText>
+                    <Select>
+                        <Option Selected>Newest</Option>
+                        <Option>Price (asc)</Option>
+                        <Option>Price (desc)</Option>
+                    </Select>
+                </Filter>
+            </FilterContainer>
+
+            <Products/>
+            <Newsletter/>
+            <Footer/>
+            
+        </Container>
+    )
+}
+
+export default styles
