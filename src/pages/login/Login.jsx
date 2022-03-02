1f56dbfe922dd18c9b6937f3781de355e6cfd85f
ValberJunior <valberjr1992@gmail.com>
2022-03-02 17:48:49 -0300

    install react-redux toolkit // Configurate login page and cart page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,24 @@
 import { Button } from "../../components"
-import { BtnGroup, Container, Form, Input, Link, Title, Wrapper } from "./styles";
+import { BtnGroup, Container, Error, Form, Input, Link, Title, Wrapper } from "./styles";
 import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { login } from "../../redux/apiCalls";
+import { useDispatch, useSelector } from "react-redux";
 
 
 const Login = () => {
 
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const  dispatch = useDispatch();
+    const {isFetching,error} = useSelector(state=>state.user);
+
     const Navigate = useNavigate();
+
+    const handleClick = (e)=>{
+        e.preventDefault();
+        login( dispatch, { username, password} );
+    };
 
     return (
         <Container>
@@ -13,14 +26,37 @@ const Login = () => {
                 <Title>SIGN IN </Title>
 
                 <Form>
-                    <Input placeholder="username" />
-                    <Input placeholder="password" />
+                    <Input 
+                    placeholder="username" 
+                    onChange={(e)=> setUsername(e.target.value)}
+                    />
+
+                    <Input
+                    placeholder="password"
+                    type="password"
+                    onChange={(e)=> setPassword(e.target.value)}
+                    />
 
                     <BtnGroup>
-                        <Button onClick={()=>{ Navigate('*') }}>LOGIN</Button>
-                        <Button onClick={()=>{ Navigate('/') }}>BACK</Button>
+
+                        <Button
+                        onClick={handleClick}
+                        disable={isFetching}
+                        >
+                            LOGIN
+                        </Button>
+
+                        <Button 
+                        onClick={()=>{ Navigate('/') }}
+                        >
+                            BACK
+                        </Button>
+
                     </BtnGroup>
                     
+                    { error && <Error>
+                            Something went wrong...
+                    </Error>}
 
                     <Link onClick={()=>{ Navigate('*') }} >DO NOT YOU REMEMBER THE PASSWORD?</Link>
                     <Link onClick={()=>{ Navigate('/register') }} >CREATE A NEW ACCOUNT</Link>
@@ -32,4 +68,3 @@ const Login = () => {
 }
 
 export default Login
-
